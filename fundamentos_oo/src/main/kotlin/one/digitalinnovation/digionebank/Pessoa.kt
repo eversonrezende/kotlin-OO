4d70bdefdaff08313e208bfb7be02d9a94a8c26b
Everson Alves de Rezende <rezende.everson@gmail.com>
2022-06-04 09:52:22 -0300

    create person class

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,5 +1,15 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Everson"
+    var cpf: String = "123.456.789-10"
+
+}
+
+fun main(){
+    val everson = Pessoa()
+
+    println(everson.nome)
+    println(everson.cpf)
 
 }
