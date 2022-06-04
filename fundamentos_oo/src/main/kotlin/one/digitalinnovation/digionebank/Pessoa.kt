8da5c051f9eaa199ca96b14a937e6feee2a933d2
Everson Alves de Rezende <rezende.everson@gmail.com>
2022-06-04 10:06:59 -0300

    updated to apply the new method pessoaInfo

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,18 +2,16 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Everson"
-
     var cpf: String = "123.456.789-10"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "Nome: $nome e CPF: $cpf"
 
 }
 
 fun main(){
     val everson = Pessoa()
-    everson.cpf = "126"
-
-    println(everson.nome)
-    println(everson.cpf)
-
-
+    println(everson.pessoaInfo())
 }
