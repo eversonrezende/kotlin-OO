492f89240f927adc99fcca0d9d15931e1f7165bc
Everson Alves de Rezende <rezende.everson@gmail.com>
2022-06-04 10:01:44 -0300

    updated to apply the private modifier to set of cpf property

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,14 +2,18 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Everson"
+
     var cpf: String = "123.456.789-10"
+    private set
 
 }
 
 fun main(){
     val everson = Pessoa()
+    everson.cpf = "126"
 
     println(everson.nome)
     println(everson.cpf)
+
 
 }
